4a949af45538a0eecfb3b667e99a2363c42e28ae
Rói á Torkilsheyggi <rt@minteo.io>
2024-09-01 15:47:41 +0100

    Multi-service, multi-environment build preseeding.

--- jenkins-preseeding/jobs.groovy
+++ jenkins-preseeding/jobs.groovy
@@ -1,26 +1,19 @@
-// scripts/jobs.groovy
+String[] services = ['frontend','user','payment','order','notification','inventory'];
+String[] env   = ['Development','Test','Production'];
 
-job('Prod-freestyle-job') {
-    description('This is an example Freestyle job created by the Job DSL plugin.')
-    scm {
-        git('https://github.com/minteo-io/demo-apps.git', 'main')
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-        shell('echo "Building the project..."')
-    }
-    publishers {
-        archiveArtifacts('**/demo/prod/*.jar')
-    }
-}
+for(envname in env) {
+    for(pipeline in services) {
+        String pipeline_capitalized = pipeline.substring(0, 1).toUpperCase() + pipeline.substring(1);
 
-pipelineJob('Prod-pipeline-job') {
-    description('This is an example Pipeline job created by the Job DSL plugin.')
-    definition {
-        cps {
-            script("""
+	// Lower-casing environment names, replacing spaces and dots.
+	String envname_lowercased = envname.substring(0,envname.length()).toLowerCase().replace(" ", "-").replace(".", "-");
+	print "Configuring " + envname + " " + pipeline_capitalized + "\n"
+
+        pipelineJob(envname + ' Configure ' + pipeline_capitalized) {
+            description("Building " + pipeline_capitalized + ' for ' + envname)
+            definition {
+                cps {
+                    script("""
                 pipeline {
                     agent any
                     stages {
@@ -42,7 +35,9 @@ pipelineJob('Prod-pipeline-job') {
                     }
                 }
             """.stripIndent())
-            sandbox(true)
+                    sandbox(true)
+                }
+            }
         }
     }
 }
