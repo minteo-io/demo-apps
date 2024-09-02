cd4e06f5b67667a9a91d11e20be955b907c41e6d
davidsundsskard <ds@minteo.io>
2024-09-02 12:20:44 +0100

    testing

--- jenkins-preseeding/jobs.groovy
+++ jenkins-preseeding/jobs.groovy
@@ -1,13 +1,13 @@
 String[] services = ['frontend','user','payment','order','notification','inventory'];
-String[] env   = ['Menning','Nároyndir','Framleiðsla'];
+String[] env   = ['Menning','Staging','Prod'];
 
 for(envname in env) {
     for(pipeline in services) {
         String pipeline_capitalized = pipeline.substring(0, 1).toUpperCase() + pipeline.substring(1);
 
-	// Lower-casing environment names, replacing spaces and dots.
-	String envname_lowercased = envname.substring(0,envname.length()).toLowerCase().replace(" ", "-").replace(".", "-");
-	print "Configuring " + envname + " " + pipeline_capitalized + "\n"
+    // Lower-casing environment names, replacing spaces and dots.
+    String envname_lowercased = envname.substring(0,envname.length()).toLowerCase().replace(" ", "-").replace(".", "-");
+    print "Configuring " + envname + " " + pipeline_capitalized + "\n"
 
         pipelineJob(envname + ' ' + pipeline_capitalized) {
             description("Building " + pipeline_capitalized + ' for ' + envname)
@@ -22,7 +22,17 @@ for(envname in env) {
                                 echo 'Building...'
                             }
                         }
-                        stage('Test') {
+                        stage('Unit Test') {
+                            steps {
+                                echo 'Testing...'
+                            }
+                        }
+                        stage('Integration Test') {
+                            steps {
+                                echo 'Testing...'
+                            }
+                        }
+                        stage('Vulnerability Scan') {
                             steps {
                                 echo 'Testing...'
                             }
